c354910c87e1bd94921dc09903d282b84470151c
Sebastian Roth <sebastian@nextbase.co.uk>
2021-08-11 10:29:42 +0100

    add test

--- test/native_resource_test.dart
+++ test/native_resource_test.dart
@@ -1,5 +1,13 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:native_resource/native_resource.dart';
 
 void main() {
-  TestWidgetsFlutterBinding.ensureInitialized();
+  group("singleton pattern", () {
+    test("It always return the same instance", () {
+      final object1 = NativeResource();
+      final object2 = NativeResource();
+
+      expect(object1 == object2, true);
+    });
+  });
 }
