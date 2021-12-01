ed2f4347eabd0897df405995fe72e1b943e5957e
Sebastian Roth <sebastian.roth@gmail.com>
2021-12-01 16:17:35 +0000

    Format workflow (#5)
    * formatting, publishable
    
    * update pubspec
    
    * no underscores in package name
    
    * package name
    
    * Correct the package path
    
    * final changes
    
    * resolve ios drive
    
    * Use different GH action
    
    * remove unused import

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,12 @@
+## 0.3.1
+
+* Correct java package name (no underscores)
+
 ## 0.3.0
 
 * Read raw values from the platform
+* Android: Move to `mavenCentral` from `jcenter`
+
 ## 0.2.0
 
 * Migrate project & activate integration tests on Github
--- ios/Classes/SwiftNativeResourcePlugin.swift
+++ ios/Classes/SwiftNativeResourcePlugin.swift
@@ -7,7 +7,7 @@ public class SwiftNativeResourcePlugin: NSObject, FlutterPlugin {
         let instance = SwiftNativeResourcePlugin()
         registrar.addMethodCallDelegate(instance, channel: channel)
     }
-    
+
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         switch call.method {
         case "read":
@@ -21,51 +21,51 @@ public class SwiftNativeResourcePlugin: NSObject, FlutterPlugin {
             break
         }
     }
-    
+
     func handleReadRaw(_ call: FlutterMethodCall, _ result: @escaping FlutterResult) {
         guard let path = Bundle.main.path(forResource: call.arguments as? String, ofType: nil) else {
             result(FlutterError(code: "invalid-params", message: nil, details: nil))
             return
         }
-        
+
         result(NSData(contentsOfFile: path))
     }
-    
+
     func handleRead(_ call: FlutterMethodCall, _ result: @escaping FlutterResult) {
-        guard let map: [String: Any] = call.arguments as? [String:Any],
+        guard let map: [String: Any] = call.arguments as? [String: Any],
             let key = map["ios-plist-key"] as? String else {
                 result(FlutterError(code: "invalid-params", message: nil, details: nil))
                 return
         }
-        
+
         if let infoFile = map["ios-plist-file"] as? String {
             guard let path = Bundle.main.path(forResource: infoFile, ofType: "plist") else {
                 result(FlutterError(code: "invalid-file-path", message: infoFile, details: nil))
                 return
             }
-            
+
             guard let dictionary = NSDictionary.init(contentsOfFile: path) else {
                 result(FlutterError(code: "invalid-file", message: infoFile, details: nil))
                 return
             }
-            
+
             guard let value = dictionary[key] else {
                 result(FlutterError(code: "invalid-key", message: infoFile, details: nil))
                 return
             }
-            
+
             result("\(value)")
         } else {
             guard let dictionary = Bundle.main.infoDictionary else {
                 result(FlutterError(code: "invalid-file", message: "", details: nil))
                 return
             }
-            
+
             guard let value = dictionary[key] else {
                 result(FlutterError(code: "invalid-key", message: "", details: nil))
                 return
             }
-            
+
             result("\(value)")
         }
     }
