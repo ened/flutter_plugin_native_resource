040663d9217a31436b490b18166886e822ae1ac8
Sebastian Roth <sebastian@nextbase.co.uk>
2021-12-01 16:01:03 +0000

    remove unused import

--- example/integration_test/native_resource_integration_test.dart
+++ example/integration_test/native_resource_integration_test.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:native_resource/native_resource.dart';
